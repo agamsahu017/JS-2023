c5a881c3959876ff8ae4184e7db0c9275010b438
Agam Sahu <agamsahu017@gmail.com>
2023-06-04 13:33:08 +0530

    Update dataStructure.js
    Anagram using objects

--- dataStructure.js
+++ dataStructure.js
@@ -34,6 +34,7 @@ console.log(fib(3))
 ///////////////////////////////////////
 
 const fib=(n)=> (n<=1 ? n : fib(n-1)+fib(n-2))
+console.log(fib(8))
 
 ////////////////// valid Anagram
 
@@ -44,4 +45,27 @@ const isAnagram=(s,t)=>{
 }
 console.log(isAnagram("anagram","nagaram"));
 console.log(isAnagram("rat","car"));
-console.log(fib(8))
+/////////////////////////////
+
+const isAnagram=(s,t)=>{
+if(s.length !== t.length) return false
+//create a object
+let obj1={};
+let obj2={};
+
+for (let i=0; i<s.length;i++){
+  obj1[s[i]]=(obj1[s[i]] || 0)+1
+  obj2[t[i]]=(obj2[t[i]] || 0)+1
+}
+for (const key in obj1){
+  if(obj1[key] !== obj2[key]){
+return false
+  }
+}
+
+return true
+}
+console.log(isAnagram("anagram","nagaram"))
+console.log(isAnagram("rat","car"))
+console.log(isAnagram("rat","tar"))
+console.log(isAnagram("ratu","taro"))
