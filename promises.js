93f247a4599cfc04b2ffe343c1365913410c6eed
Agam Sahu <agamsahu017@gmail.com>
2023-08-19 01:28:01 +0530

    promises

--- promises.js
+++ promises.js
@@ -1,1 +1,19 @@
-console.log("hi promise");
+console.log("start");
+// console.log(Promise.resolve("hi there"))
+const sub = Promise.resolve("hi there");
+const sub1 = Promise.reject("this reject");
+
+console.log("7 ", sub);
+console.log("8 ", sub1);
+
+sub.then((res)=>{
+    console.log(res);
+})
+
+sub1.then((res)=>{
+    console.log(res);
+}).catch((x)=>{
+    console.log(x);
+})
+
+console.log("end");
